7a60052716ca446115cc55acd32cc125f3e091ce
Faisal <saleemfs27@gmail.com>
2022-02-22 13:59:54 +0200

    Set up Dockerfile for server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,13 @@ const express = require('express')
 
 const app = express()
 
-app.post('/alert', async () => {})
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-app.listen(5000, () => console.log('Running at http://localhost:5000'))
+app.post('/alert', async (req, res) => {
+  res.json(req.body)
+})
+
+app.listen(5000, () => {
+  console.log('Running at http://localhost:5000')
+})
