c2b3dd7f01a2152feb32be2840325843e72c2056
Faisal <saleemfs27@gmail.com>
2022-02-23 20:40:14 +0200

    format

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+
 const userRoutes = require('./routes/user.routes')
 const providerRoutes = require('./routes/provider.routes')
 const panicRoutes = require('./routes/panic.routes')
