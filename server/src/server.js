3cc5d84c0497c113722d963ddf4a31ad82217aea
Faisal <saleemfs27@gmail.com>
2022-02-24 19:07:59 +0200

    Added cors

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 
 const userRoutes = require('./routes/user.routes')
 const providerRoutes = require('./routes/provider.routes')
@@ -9,6 +10,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
 
 app.use('/user', userRoutes)
 app.use('/provider', providerRoutes)
